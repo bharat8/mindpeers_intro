50d9cde6a350951c0b1bf07f7c05caa136bd357e
Bharat Sundal <bharatsundal8@gmail.com>
2022-10-21 11:16:46 +0530

    minor fixes v1

--- lib/features/onboarding/views/screens/begin_journey_screen.dart
+++ lib/features/onboarding/views/screens/begin_journey_screen.dart
@@ -7,6 +7,7 @@ import 'package:mindpeers_intro/features/core/widgets/loading_indicator.dart';
 import 'package:mindpeers_intro/features/core/widgets/primary_button.dart';
 import 'package:mindpeers_intro/features/onboarding/bloc/onboarding_bloc.dart';
 import 'package:mindpeers_intro/features/onboarding/views/screens/user_mental_health_data.dart';
+import 'package:sizer/sizer.dart';
 
 class BeginJourneyScreen extends StatelessWidget {
   const BeginJourneyScreen({super.key});
@@ -36,55 +37,59 @@ class BeginJourneyScreen extends StatelessWidget {
               child: SafeArea(
                 child: Align(
                   alignment: Alignment.bottomLeft,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                  child: Stack(
                     children: [
-                      Expanded(
+                      SizedBox(
+                        height: 40.h,
                         child: SvgPicture.asset(
                           'assets/images/mountain-withball.svg',
                           fit: BoxFit.cover,
                         ),
                       ),
-                      verticalSpaceLarge,
-                      verticalSpaceLarge,
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: UIConstants.baseMargin * 4,
-                        ),
-                        child: Column(
-                          children: [
-                            Text(
-                              'MindPeers Signature\nMental Strength Test',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headlineSmall!
-                                  .copyWith(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                              textAlign: TextAlign.center,
-                            ),
-                            verticalSpaceRegular,
-                            Text(
-                              'Discover your strengths and identify '
-                              'growth areas in under 4 minutes',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .titleMedium!
-                                  .copyWith(
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                              textAlign: TextAlign.center,
-                            ),
-                            verticalSpaceLarge,
-                            PrimaryButton(
-                              label: 'Begin journey',
-                              onPress: () {
-                                context.read<OnboardingBloc>().add(LoadQuiz());
-                              },
-                            ),
-                            verticalSpaceLarge,
-                          ],
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: UIConstants.baseMargin * 4,
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                'MindPeers Signature\nMental Strength Test',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headlineSmall!
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                textAlign: TextAlign.center,
+                              ),
+                              verticalSpaceRegular,
+                              Text(
+                                'Discover your strengths and identify '
+                                'growth areas in under 4 minutes',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                textAlign: TextAlign.center,
+                              ),
+                              verticalSpaceLarge,
+                              PrimaryButton(
+                                label: 'Begin journey',
+                                onPress: () {
+                                  context
+                                      .read<OnboardingBloc>()
+                                      .add(LoadQuiz());
+                                },
+                              ),
+                              verticalSpaceLarge,
+                            ],
+                          ),
                         ),
                       ),
                     ],
